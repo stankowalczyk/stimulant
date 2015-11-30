107c5d94e4308c0e80e7580d1507439c8a2422e2
Luke Horvat <lukehorvat@gmail.com>
2015-11-30 13:24:49 +1100

    Bump deps

--- src/scripts/navbar/index.jsx
+++ src/scripts/navbar/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import {Navbar as BsNavbar, NavBrand, Nav, NavItem} from "react-bootstrap";
+import {Navbar as BsNavbar, Nav, NavItem} from "react-bootstrap";
 
 export class Navbar extends React.Component {
   render() {
     return (
       <BsNavbar fixedTop fluid>
-        <NavBrand><a href="#/">App</a></NavBrand>
-        <Nav right>
+        <BsNavbar.Header>
+          <BsNavbar.Brand><a href="#/">App</a></BsNavbar.Brand>
+        </BsNavbar.Header>
+        <Nav pullRight>
           <NavItem href="#/sign-out"><i className="fa fa-sign-out"></i> Sign Out</NavItem>
         </Nav>
       </BsNavbar>
