01dd29389e901ea9a434fd92d6df53365eaa5480
Luke Horvat <lukehorvat@gmail.com>
2016-04-11 12:58:27 +1000

    CONFIG: Pass apiUrl to gulpfile config

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
-node_modules/stimulant-gulpfile/dist/index.js
+const gulpfile = require("stimulant-gulpfile");
+
+Object.assign(gulpfile.config, {
+  development: {
+    apiUrl: "http://localhost:3000"
+  },
+  staging: {
+    apiUrl: "http://localhost:3001"
+  },
+  production: {
+    apiUrl: "http://localhost:3002"
+  }
+}[gulpfile.config.env]);
