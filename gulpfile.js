bca7ffbcc7cbf6db721c6a0335f4df0e8fed1aec
Luke Horvat <lukehorvat@gmail.com>
2016-04-13 17:04:15 +1000

    CONFIG: Define env-dependent API urls in src/scripts/environment, not gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,1 @@
-const gulpfile = require("stimulant-gulpfile");
-
-Object.assign(gulpfile.config, {
-  development: {
-    apiUrl: "http://localhost:3000"
-  },
-  staging: {
-    apiUrl: "http://localhost:3001"
-  },
-  production: {
-    apiUrl: "http://localhost:3002"
-  }
-}[gulpfile.config.env]);
+node_modules/stimulant-gulpfile/dist/index.js
--- src/scripts/environment/index.jsx
+++ src/scripts/environment/index.jsx
@@ -1,1 +1,11 @@
-export const ENV = $ENV;
+export const ENV = {
+  development: {
+    apiUrl: "http://localhost:3000"
+  },
+  staging: {
+    apiUrl: "http://localhost:3001"
+  },
+  production: {
+    apiUrl: "http://localhost:3002"
+  }
+}[process.env.NODE_ENV];
